b24c2cd3ba76cef122b383966484939b75b338cd
= <=>
2023-12-30 09:17:05 +0700

    fix answer

--- src/App.js
+++ src/App.js
@@ -1,86 +1,107 @@
 import { useEffect, useRef, useState } from "react";
-import axios from "axios"
+import axios from "axios";
 
 function App() {
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState("");
-  const [sentence, setSentence] = useState([])
-  const [content, setContent] = useState([])
-  const count = useRef(0)
+  const [sentence, setSentence] = useState([]);
+  const [content, setContent] = useState([]);
+  const [answer, setAnswer] = useState("");
+  const count = useRef(0);
   useEffect(() => {
-    if (!content || content.length === 0) return
+    if (!content || content.length === 0) return;
     const timer = setInterval(() => {
-      if (!content[count.current]) {
-        clearInterval(timer)
-        count.current = 0
-        return
+      if (count.current >= content.length) {
+        clearInterval(timer);
+        count.current = 0;
+        return;
       }
-      setQuery(prev => prev + content[count.current++])
-    }, 300);
-    return () => clearInterval(timer)
-  }, [content])
+      setAnswer((prev) => prev + content[count.current++]);
+    }, 50);
+    return () => clearInterval(timer);
+  }, [content]);
 
   const handleSend = async () => {
-    if (query === '') return
+    if (query === "") return;
     try {
       setLoading(true);
-      setSentence(prev => [
+      setSentence((prev) => [
         ...prev,
         {
-          role: 'user',
-          content: query
-        }
-      ])
+          role: "user",
+          content: query,
+        },
+      ]);
       const payload = {
         stream: true,
         model: "mistralai/mixtral-8x7b-instruct",
         max_tokens: 300,
         temperature: 0.88,
         top_p: 1,
-        messages: sentence
-      }
-      const response = await axios.post('http://localhost:8080/api/v1/rpa/open-ai', JSON.stringify(payload), {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        // responseType: 'stream'
-      })
-      // const response = await axios.post('https://openrouter.ai/api/v1/chat/completions', JSON.stringify(payload), {
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //     'Authorization': 'Bearer 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'
-      //   },
-      //   // responseType: 'stream'
-      // })
+        messages: [
+          ...sentence,
+          {
+            role: "user",
+            content: query,
+          },
+        ],
+      };
+      // console.log(query);
+      // console.log(sentence);
+      console.log(payload);
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/rpa/open-ai",
+        JSON.stringify(payload),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      // const response = await axios.post(
+      //   "https://openrouter.ai/api/v1/chat/completions",
+      //   JSON.stringify(payload),
+      //   {
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //       Authorization:
+      //         "Bearer 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",
+      //     },
+      //   }
+      // );
       const data = response.data;
-      data.split('\n\n').forEach((event) => {
-        if (event.startsWith('data:')) {
+      // console.log(data);
+      data.split("\n\n").forEach((event) => {
+        if (event.startsWith("data:")) {
           try {
-            const eventJson = JSON.parse(event.replace('data: ', ''));
+            const eventJson = JSON.parse(event.replace("data: ", ""));
             eventJson.choices.forEach((choice) => {
-              setContent(prev => [...prev, choice.delta.content])
+              console.log(choice);
+              setContent((prev) => [...prev, choice.delta.content]);
             });
           } catch (error) {
             console.error("Lỗi khi phân tích cú pháp JSON", error);
           }
         }
-      })
+      });
     } catch (error) {
       console.error("Error fetching data:", error);
       setErrors(error.message);
     } finally {
       setLoading(false);
-      setQuery('')
+      setQuery("");
     }
   };
+
+  // console.log(content);
 
   return (
     <div className="h-full flex justify-center items-center">
       <div className="p-5 mx-auto w-1/2 shadow-9xl rounded-lg h-[90vh] relative">
         <div className="w-[calc(100%-40px)] flex justify-center items-center gap-x-3 mb-5 absolute bottom-0">
           <button className="hover:shadow-8xl transition-all w-[30px] h-[30px] rounded-full">
-            icon
+            Chat
           </button>
           <input
             type="text"
@@ -92,7 +113,7 @@ function App() {
             onClick={handleSend}
             disabled={loading}
             style={{ opacity: loading ? "0.2" : "" }}
-            className="px-5 py-1 bg-primary-gradient rounded-md shadow-8xl font-semibold text-white text-lg select-none hover:shadow-none hover:scale-95 transition-all flex-shrink-0"
+            className="px-5 py-1 bg-gray-800 rounded-md shadow-8xl font-semibold text-white text-lg select-none hover:shadow-none hover:scale-95 transition-all flex-shrink-0"
           >
             Send
           </button>
@@ -103,15 +124,15 @@ function App() {
         {!loading && errors && (
           <p className="font-semibold text-xl text-red-500">{errors}</p>
         )}
-        {!loading && (
-          <div className="flex flex-col gap-3 h-[90%] overflow-auto">
-            {!!sentence &&
-              sentence.length > 0 &&
-              sentence.map((sentence, index) => (
-                <Sentence key={index} {...sentence} />
-              ))}
-          </div>
-        )}
+        <div className="flex flex-col gap-3 h-[10%] overflow-auto">
+          {!!sentence &&
+            sentence.length > 0 &&
+            sentence.map((sentence, index) => (
+              <Sentence key={index} {...sentence} />
+            ))}
+        </div>
+
+        {answer && <Sentence role="Open AI" content={answer} />}
       </div>
     </div>
   );
